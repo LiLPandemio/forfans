14adf51636b7264adab5c5da6f0e05dc20f78b8b
LiLPandemio <markhaxx@gmail.com>
2021-11-04 11:48:21 +0100

    Comentarios

--- www/index.php
+++ www/index.php
@@ -4,7 +4,16 @@
 // Marc Moreno & Bryan Medrano  *
 // ******************************
 
+//Path absoluto a la raiz de la app web
 define('ROOT', getcwd());
+//Carga de las funciones esenciales
 require_once(ROOT."/functions/system.php");
-echo "Site under development";
+
+
+if (isset($_REQUEST['page'])) {
+    // El usuario esta pidiendo una pagina en especifico
+} else {
+    // El usuario no pide ninguna pagina, mostrar landing o home si esta autenticado
+}
+
 ?>
