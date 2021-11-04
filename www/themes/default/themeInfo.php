3ad9441b44c25225d1b5a5235ed3ccf144f47839
LiLPandemio <markhaxx@gmail.com>
2021-11-04 11:53:38 +0100

    Default theme info file

---
+++ www/themes/default/themeInfo.php
@@ -0,0 +1,6 @@
+<?php
+$themeInfo["ThemeName"]     = "ForFans Classic";
+$themeInfo["ThemeIcon"]     = "icon.png";
+$themeInfo["ThemeDescri"]   = "El tema por defecto de forfans";
+$themeInfo["ThemeAuthor"]   = "LiLPandemio";
+?>
