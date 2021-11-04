b422a96e8786a8409bf59d474e1c8639f434879f
LiLPandemio <markhaxx@gmail.com>
2021-11-04 11:28:32 +0100

    Define root and set developers comments

--- www/index.php
+++ www/index.php
@@ -1,3 +1,10 @@
 <?php
+// ******************************
+// ForFans Project - Bemen3     *
+// Marc Moreno & Bryan Medrano  *
+// ******************************
+
+define('ROOT', getcwd());
+require_once(ROOT."/functions/system.php");
 echo "Site under development";
 ?>
