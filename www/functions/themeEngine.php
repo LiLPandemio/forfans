0ffb4cf7533e238dff6a32bfac6ec3c24d3aabbb
LiLPandemio <markhaxx@gmail.com>
2021-11-05 12:21:42 +0100

    Theme engine

--- www/functions/themeEngine.php
+++ www/functions/themeEngine.php
@@ -1,3 +1,42 @@
 <?php
 //Aqui estaran las funciones relacionadas con los temas de la pagina, Consultar modificar etc
-?>
+
+
+// Comprueba si en el tema actual existe la pagina que el usuario solicita, devolvera 0 en caso de error o que no exista y 1 si existe
+function pageExists ($page) {
+    require_once(ROOT."/config.php");
+    //limpiamos el input para evitar posibles vulnerabilidades
+    $page = preg_replace('/[^A-Za-z0-9\-]/', '', $page);    // Reemplaza por nada caracteres que no sean A-Z a-z 0-9
+    if ($page = "") {                                       //Comprueba que la pagina no este en blanco
+        return 0;                                           //Se pide la pagina "", se retorna falso
+    } else {
+        //Comprueba que exista el archivo
+        if (file_exists(ROOT."/themes/".$config['theme']."/$page/index.php")) {  //Si la pagina existe, 
+            return true;                                    //Devuelve TRUE
+        } else {                                            //Si no existe
+            return false;                                   //Devuelve false
+        }
+        return false;                                       //Retorna falso si no se puede ejecutar el condicional de arriba
+    }
+    return false;                                           //Retorna falso si no se puede ejecutar el conficional de arriba
+}
+
+//Esta funcion carga una pagina del tema actual en $config["theme"]
+function loadPage ($page, $theme = "default") {
+
+    require_once(ROOT."/themes/$theme/head/index.php"); //Carga el archivo principal del tema de la pagina que se solicitó.
+    require_once(ROOT."/themes/$theme/".$page."/index.php"); //Carga el archivo principal del tema de la pagina que se solicitó.
+}
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+    
+// </body>
+// </html>
--- www/index.php
+++ www/index.php
@@ -4,38 +4,49 @@
 // Marc Moreno & Bryan Medrano  *
 // ******************************
 
-//Path absoluto a la raiz de la app web
+//DEBUG - MUESTRA LOS ERRORES EN EL NAVEGADOR
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Establece el path absoluto a la raiz de la app web de forma dinamica
 define('ROOT', getcwd());
+
 //Carga de las funciones esenciales
 require_once(ROOT . "/functions/system.php");
-
+require_once(ROOT . "/functions/themeEngine.php");
 
 if (isset($_REQUEST['page'])) {
     // El usuario esta pidiendo una pagina en especifico
-    switch ($REQUEST['page']) {
+    switch ($_REQUEST['page']) {
             // Redirecciones especiales de la app (API, Admin)
         case 'api':
             //Redireccionar a sistema API
+            echo "Getting api";
             break;
         case 'webmap':
             //Obtener webmap
+            echo "Getting webmap";
             break;
         case 'adminpanel':
             //Panel de administrador
+            echo "Getting adminpanel";
             break;
 
         //Paginas del tema
 
         default:
             //Cargar pagina del tema actuar, si no hay, cargar perfil de usuario, si no hay, 404
-            if (pageExists($REQUEST['page'])) {
+            if (pageExists($_REQUEST['page'])) {
+                require(ROOT."/config.php");
+                loadPage($_REQUEST["page"], $config['theme']);
                 //La pagina existe en el tema actual
             } else {
-                if (profileExists($REQUEST['page'])){
-                    //El perfil existe
-                } else {
-                    //404
-                }
+                // if (profileExists($REQUEST['page'])){
+                //     //El perfil existe
+                // } else {
+                //     //404
+                // }
             }
             
             break;
