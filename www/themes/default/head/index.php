782ba3c8b45690cad4b1ee5b300c03116fbbf193
LiLPandemio <markhaxx@gmail.com>
2021-11-06 00:55:57 +0100

    Removing unused code used for testing

--- www/themes/default/head/index.php
+++ www/themes/default/head/index.php
@@ -1,23 +1,14 @@
 <?php
 require(ROOT . "/config.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
 <head>
 	<meta charset="UTF-8">
+	<meta name="description" content="<?php echo $config[""]?>">
+	<meta name="keywords" content="<?php echo $config[""]?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="<?php echo $config["fullsiteurl"]."themes/".$config['theme']."/" ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<link rel="stylesheet" href="<?php echo $config["fullsiteurl"]."themes/".$config['theme']."/" ?>assets/bootstrap/css/bootstrap.min.css">
+	<script src="<?php echo $config["fullsiteurl"] . "themes/" . $config['theme'] . "/" ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="<?php echo $config["fullsiteurl"] . "themes/" . $config['theme'] . "/" ?>assets/bootstrap/css/bootstrap.min.css">
 	<title><?php echo $config["sitename"] . " - " . $_REQUEST["page"]; ?></title>
 </head>
-
-<body>
-	<a class="btn btn-primary btn-lg" href="#" role="button">ASDASD</a>
-</body>
-
-<footer>
-</footer>
-
-</html>
