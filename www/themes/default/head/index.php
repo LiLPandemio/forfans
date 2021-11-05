dc24e78bd12de9ef82a6b2553ad4470b84dd8424
LiLPandemio <markhaxx@gmail.com>
2021-11-05 13:24:54 +0100

    Multi theme setup headers

--- www/themes/default/head/index.php
+++ www/themes/default/head/index.php
@@ -1,6 +1,23 @@
 <?php
-require(ROOT."/config.php");
+require(ROOT . "/config.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<title><?php echo $config["sitename"]." - ".$_REQUEST["page"]; ?></title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="<?php echo $config["fullsiteurl"]."themes/".$config['theme']."/" ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="<?php echo $config["fullsiteurl"]."themes/".$config['theme']."/" ?>assets/bootstrap/css/bootstrap.min.css">
+	<title><?php echo $config["sitename"] . " - " . $_REQUEST["page"]; ?></title>
 </head>
+
+<body>
+	<a class="btn btn-primary btn-lg" href="#" role="button">ASDASD</a>
+</body>
+
+<footer>
+</footer>
+
+</html>
