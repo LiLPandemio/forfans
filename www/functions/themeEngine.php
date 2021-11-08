e3d9d183f79321d54dbfcd3d0a0f87f956dfd331
LiLPandemio <markhaxx@gmail.com>
2021-11-08 12:53:57 +0100

    Clean comments

--- www/functions/themeEngine.php
+++ www/functions/themeEngine.php
@@ -26,16 +26,3 @@ function loadPage ($page, $theme = "default") {
     require(ROOT."/themes/$theme/head/index.php");      //Carga el head y las primeras lineas HTML del tema.
     require(ROOT."/themes/$theme/".$page."/index.php"); //Carga el archivo principal de la pagina en el tema que se solicitó.
 }
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-// </head>
-// <body>
-    
-// </body>
-// </html>
