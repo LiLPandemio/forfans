19ca1c16d29999e615f1e952aff83d85f0a8545a
LiLPandemio <markhaxx@gmail.com>
2021-11-09 15:13:06 +0100

    Added lang keys

--- www/locale/es.php
+++ www/locale/es.php
@@ -7,15 +7,27 @@ function lang($phrase)
     
     $lang['welcome_to']
         = "Bienvenido a {sitename}!";
+    
+    $lang['welcome_back']
+        = "Bienvenido de nuevo!";
 
     $lang['login']
         = "Iniciar sesion";
+
+    $lang['login_subtitle']
+        = "¿Ya tienes una cuenta?<br/>¡Inicia sesion!";
+
+    $lang['register_subtitle']
+        = "¿Aun no tienes cuenta?<br/>¡Crea una!";
 
     $lang['register']
         = "Registrarse";
 
     $lang['join_site']
         = "Unirme a {sitename}";
+
+    $lang['create_account']
+        = "Crear cuenta";
 
     $str = str_replace("{sitename}", $config["sitename"], $lang[$phrase]);
     return $str;
