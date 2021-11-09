ea81ef36e1f073eeee12cae0e4cd8ba5a0429527
LiLPandemio <markhaxx@gmail.com>
2021-11-09 15:13:17 +0100

    Designing auth

--- www/themes/default/auth/index.php
+++ www/themes/default/auth/index.php
@@ -4,47 +4,38 @@
 
 <body>
     <div class="container" id="container">
+        <div class="site-controllers">
+        <i id="settings-icon" class="fa-solid fa-gear"></i>
+        </div>
         <div class="form-container sign-up-container">
             <form action="#">
-                <h1>Create Account</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your email for registration</span>
+                <h1><?=lang("create_account")?></h1>
                 <input type="text" placeholder="Name" />
                 <input type="email" placeholder="Email" />
                 <input type="password" placeholder="Password" />
-                <button>Sign Up</button>
+                <button><?=lang("register")?></button>
             </form>
         </div>
         <div class="form-container sign-in-container">
             <form action="#">
-                <h1>Sign in</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your account</span>
+                <h1><?=lang("login")?></h1>
                 <input type="email" placeholder="Email" />
                 <input type="password" placeholder="Password" />
                 <a href="#">Forgot your password?</a>
-                <button>Sign In</button>
+                <button><?=lang("login")?></button>
             </form>
         </div>
         <div class="overlay-container">
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button class="ghost" id="signIn">Sign In</button>
+                    <h1><?=lang("welcome_to")?></h1>
+                    <p><?=lang("login_subtitle")?></p>
+                    <button class="ghost" id="signIn"><?=lang("login")?></button>
                 </div>
                 <div class="overlay-panel overlay-right">
-                    <h1><?=lang("welcome_to")?></h1>
-                    <p>Enter your personal details and start journey with us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
+                    <h1><?=lang("welcome_back")?></h1>
+                    <p><?=lang("register_subtitle")?></p>
+                    <button class="ghost" id="signUp"><?=lang("register")?></button>
                 </div>
             </div>
         </div>
