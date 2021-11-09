57ad2fbd65c3f39febee4e33d67a1362f347e1a9
LiLPandemio <markhaxx@gmail.com>
2021-11-09 15:12:59 +0100

    Cache control

--- www/index.php
+++ www/index.php
@@ -9,6 +9,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//NO CACHE
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 //Establece el path absoluto a la raiz de la app web de forma dinamica
 define('ROOT', getcwd());
 
