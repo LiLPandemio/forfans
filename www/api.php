016e11154acdfef160a70a54851d6fd95b10c1bf
LiLPandemio <admin@societyplus.net>
2021-11-10 13:41:53 +0100

    Commit

--- www/api.php
+++ www/api.php
@@ -3,14 +3,16 @@
 if (isset($param['1'])) { //Si existe un segundo parametro para la api
     switch ($param['1']) {
         case 'login':
+            require_once(ROOT."/functions/authEngine.php");
             //provide user pass via post and login
-            if (isset($_POST['username']) and isset($_POST['password'])) {
-                echo login($_POST['username'], $_POST['password']);
+            if (isset($_REQUEST['username']) and isset($_REQUEST['password'])) {
+                echo login($_REQUEST['username'], $_REQUEST['password']);
             } else {
                 # code...
             }
             
             break;
+        
         
         default:
             echo "something went wrong";
--- www/functions/authEngine.php
+++ www/functions/authEngine.php
@@ -2,26 +2,31 @@
 // Este archivo contine las funciones que se usan para autenticar a un usuario, getionar las cuentas eliminar crear etc.
 
 //*Esta funcion devuelve un token o false si las credenciales no coinciden
-function login($username, $password){
-    require_once(ROOT. "/functions/db.php");                        //Importar la base de datos
+function login($username, $password)
+{
+    require(ROOT . "/functions/db.php");                       //Importar la base de datos
     $username = str_replace(" ", "", $username);                    //Limpiar espacios en blanco
     if ($username !== "") {                                         //Si el nombe de usuario no esta en blanco
         if ($password !== "") {                                     //Y la contraseña no esta en blanco
-            
+
             //Comprobar login
-            $stmt = $conn->prepare("SELECT * FROM 'usuarios' WHERE `correo` = ?");  //Buscar en la BD
-            $stmt->bind_param("s", $username);                                      //Por el correo
-
-            $user = $stmt->count();
-
-            var_dump($user);
+            $stmt = $conn->prepare("SELECT * FROM `usuarios` WHERE `correo` = ? LIMIT 1;"); //Buscar en la BD
+            $stmt->bindParam("s", $username);
+            $stmt->execute(array($username));
+            $user = $stmt -> fetchAll();
             
         } else {
             json_encode(array("response" => "username_password"));
         }
-        
     } else {
         json_encode(array("response" => "username_error"));
     }
-    
+}
+
+//*Esta funcion genera un token aleatorio de 80 caracteres
+function tokenGenerator()
+{
+    $token = openssl_random_pseudo_bytes(80);
+    $token = bin2hex($token);
+    return $token;
 }
