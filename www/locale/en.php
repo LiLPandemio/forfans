6cfecad39451190fa08891a51cc0a5c8da77b87a
LiLPandemio <markhaxx@gmail.com>
2021-11-15 16:02:12 +0100

    English added

--- www/locale/en.php
+++ www/locale/en.php
@@ -1,3 +1,24 @@
 <?php
 //Aqui se almacenan las claves con las cadenas de texto para el idioma INGLES
-?>
+
+function lang($langkey)
+{
+    require(ROOT."/config.php");
+    $lang['welcome_to']
+        = "Welcome to {sitename}!";
+
+    $lang['login']
+        = "Login";
+
+    $lang['register']
+        = "Register";
+
+    $lang['join_site']
+        = "Join {sitename}";
+
+    var_dump($lang);
+
+    $text = str_replace("{sitename}", $config['sitename'], $lang[$langkey]);
+
+    return $text;
+}
