becfc45ec2164995ff30521d6cce227df31103e9
LiLPandemio <markhaxx@gmail.com>
2021-12-02 12:17:15 +0100

    Added templates for sites with and without account

---
+++ www/themes/default/template/Authorization-req.php
@@ -0,0 +1,41 @@
+<?php
+//Auth required!
+require(ROOT . "/config.php");
+require(ROOT . "/locale/" . $config["default_lang"] . ".php");
+
+if (isset($_COOKIE['token'])) {                         //SI EXISTE LA COOKIE CON TOKEN
+    $token = $_COOKIE['token'];                         //GUARDALA EN $token
+    require ROOT . "/functions/authEngine.php";         //IMPORTA LAS FUNCIONES DE AUTENTICACION
+    if (checkTokenStatus($token) != "INVALID_TOKEN") {  //Si el estado del token no es invalido
+        //Mostrar la pagina
+        ?>
+<!-- Aqui puedes añadir contenido exta a head -->
+
+<!-- Hasta aqui el contenido head -->
+</head>
+
+<body>
+    <!-- Aqui va el contenido principal de la pagina -->
+    <!-- Load navbar -->
+    <?php require(ROOT . "/themes/" . $config["theme"] . "/components/navbar.phtml"); ?>
+    <div class="row">
+    </div>
+    <div class="row">
+        <div style="background-color: #ff8888;" class="col-sm-3">
+            Menu template
+        </div>
+        <div style="background-color: #88ff88;" class="col-sm-6">
+            Main content template
+        </div>
+        <div style="background-color: #8888ff;" class="col-sm-3">
+            Menu template
+        </div>
+    </div>
+</body>
+        <?php
+    } else {
+        echo "SESSION ERROR X1";
+    }
+} else {
+    echo "SESSION ERROR X2";
+}
