01808ffd62f4ee62bd626bca182991a7de2bea2c
LiLPandemio <markhaxx@gmail.com>
2021-12-13 16:13:36 +0100

    Commented function redirect

--- www/functions/utils1.php
+++ www/functions/utils1.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Esta funcion redirige a una pagina interna.
+ *  - Para usar solo hace falta especificar la pagina de destino.
+ *  - Por defecto redirige a auth.
+ */
 function redirect($page = "auth"){
     require_once(ROOT . "/functions/themeEngine.php");
     $nexturl = $config['fullsiteurl'].$page;
