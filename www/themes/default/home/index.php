eae232127714915fb450659a31e739fd6bb62f14
LiLPandemio <markhaxx@gmail.com>
2021-12-13 16:11:58 +0100

    Home page started

--- www/themes/default/home/index.php
+++ www/themes/default/home/index.php
@@ -1,19 +1,42 @@
 <?php
 //Auth required!
-if (isset($_COOKIE['token'])) {                         //SI EXISTE LA COOKIE CON TOKEN
-    $token = $_COOKIE['token'];                         //GUARDALA EN $token
-    require ROOT . "/functions/authEngine.php";         //IMPORTA LAS FUNCIONES DE AUTENTICACION
-    if (checkTokenStatus($token) != "INVALID_TOKEN") {  //Si el estado del token no es invalido
+require(ROOT . "/config.php");                                      //Añadir la configuracion
+require(ROOT . "/locale/" . $config["default_lang"] . ".php");      //Cargar idioma
+require(ROOT."/functions/utils1.php");
+
+if (isset($_COOKIE['token'])) {                                     //SI EXISTE LA COOKIE CON TOKEN
+    $token = $_COOKIE['token'];                                     //GUARDALA EN $token
+    require ROOT . "/functions/authEngine.php";                     //IMPORTA LAS FUNCIONES DE AUTENTICACION
+    if (checkTokenStatus($token) != "INVALID_TOKEN") {              //Si el estado del token no es invalido
         //Mostrar la pagina
         ?>
+<!-- Aqui puedes añadir contenido exta a head -->
+
+<!-- Hasta aqui el contenido head -->
 </head>
+
 <body>
-    ASDASDASASD
+    <!-- Aqui va el contenido principal de la pagina -->
+    <!-- Load navbar -->
+    <?php require(ROOT . "/themes/" . $config["theme"] . "/components/navbar.phtml"); ?>
+    <div class="row">
+    </div>
+    <div class="row">
+        <div style="background-color: #ff8888;" class="col-sm-3">
+            Menu template
+        </div>
+        <div style="background-color: #88ff88;" class="col-sm-6">
+            Main content template
+        </div>
+        <div style="background-color: #8888ff;" class="col-sm-3">
+            Menu template
+        </div>
+    </div>
 </body>
         <?php
     } else {
-        echo "SESSION ERROR X1";
+        redirect();
     }
 } else {
-    echo "SESSION ERROR X2";
+    redirect();
 }
