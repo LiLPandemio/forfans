6120f123ba55230e9e54e868a9bfe6b8b9f9fe1d
LiLPandemio <markhaxx@gmail.com>
2021-12-14 16:24:29 +0100

    Added global.css to all files

--- www/themes/default/head/index.php
+++ www/themes/default/head/index.php
@@ -7,6 +7,7 @@ require(ROOT . "/config.php");
 	<meta name="description" content="<?php echo $config["description"]?>">
 	<meta name="keywords" content="<?php echo $config["keywords"]?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="<?php echo $config['fullsiteurl']."themes/".$config['theme'] . "/" ?>assets/css/global.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="<?php echo $config["fullsiteurl"] . "themes/" . $config['theme'] . "/" ?>assets/jquery/jquery.min.js"></script>
