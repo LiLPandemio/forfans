540ef9130f97f436bc43b45f5a6b1a7e9784097e
LiLPandemio <markhaxx@gmail.com>
2021-12-14 16:46:54 +0100

    File require duplicated

--- www/themes/default/logout/index.php
+++ www/themes/default/logout/index.php
@@ -1,15 +1,14 @@
 <?php
 //Simplemente elimina cookies y sesiones y redirige a login
+require(ROOT."/functions/utils1.php");
 if (isset($_COOKIE['token'])) {
     //Eliminar el token de la DB
     $token = $_COOKIE['token'];
     require(ROOT."/functions/authEngine.php");
-    require(ROOT."/functions/utils1.php");
     removeToken($token);
     redirect();
 }
 //No hay sesion :v
-require(ROOT."/functions/utils1.php");
 redirect();
 
 ?>
