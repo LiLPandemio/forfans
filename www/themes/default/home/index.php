4ff17d82a97c41ae6bad5b6c4648b630e05cb8c5
LiLPandemio <markhaxx@gmail.com>
2022-01-17 17:14:14 +0100

    Feed designed

--- www/themes/default/home/index.php
+++ www/themes/default/home/index.php
@@ -6,7 +6,7 @@ if (isset($_COOKIE['token'])) {                                     //SI EXISTE 
         //Mostrar la pagina
 ?>
         <!-- Aqui puedes añadir contenido exta a head -->
-        <link rel="stylesheet" href="<?=$config["fullsiteurl"]."themes/".$config["theme"]."/home/"."index"?>.css">
+        <link rel="stylesheet" href="<?= $config["fullsiteurl"] . "themes/" . $config["theme"] . "/home/" . "index" ?>.css">
 
         <!-- Hasta aqui el contenido head -->
         </head>
@@ -19,35 +19,49 @@ if (isset($_COOKIE['token'])) {                                     //SI EXISTE 
             </div>
             <div class="row" style="margin-top: 60px;">
                 <div class="col-sm-3">
-                    <?php for ($i = 0; $i < 4; $i++) { ?>
-                        <a href="#">
-                            <div class="card text-left" style="margin-bottom: 10px; margin-left: 10px; margin-right: 10px;">
-                                <div class="card-body" style="display: flex; flex-direction: row;">
-                                    <img class="avatar" style="border-radius: 50px; height: 50px; width: 50px" src="https://www.societyplus.net/upload/photos/2020/11/9FP6DaNMH4PU9Mth9pI4_06_219caf368060b509948cf08a0102afdc_avatar.jpg" alt="">
-                                    <div style="display:flex; flex-direction:column; margin-left:10px">
-                                        <div style="height: 50%;">
-                                            <p class="card-text">@admin</p>
-                                        </div>
-                                        <div style="height: 50%;">
-                                            <a style="margin:2px" class="badge badge-primary" href="#">12 Posts</a>
-                                            <a style="margin:2px" class="badge badge-success" href="#">Suscribirse</a>
-                                            <a style="margin:2px" class="badge badge-accent" href="#">Seguir</a>
+                    <div class="collapse" id="collapseExample"> 
+                        <!-- TODO: COLLAPSE SUGGESTIONS FOR MOBILE! -->
+
+                        <div class="title-wrapper" style="padding:10px">
+                            <ul class="breadcrumb">
+                                <li>Perfiles sugeridos</li>
+                            </ul>
+                        </div>
+                        <?php for ($i = 0; $i < 4; $i++) { ?>
+                            <a href="#">
+                                <div class="card text-left" style="margin-bottom: 10px; margin-left: 10px; margin-right: 10px;">
+                                    <div class="card-body" style="display: flex; flex-direction: row;">
+                                        <img class="avatar" style="border-radius: 50px; height: 50px; width: 50px" src="https://cataas.com/cat" alt="">
+                                        <div style="display:flex; flex-direction:column; margin-left:10px">
+                                            <div style="height: 50%;">
+                                                <p class="card-text">@admin</p>
+                                            </div>
+                                            <div style="height: 50%;">
+                                                <a style="margin:2px" class="badge badge-primary" href="#">12 Posts</a>
+                                                <a style="margin:2px" class="badge badge-success" href="#">Suscribirse</a>
+                                                <a style="margin:2px" class="badge badge-accent" href="#">Seguir</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                    <?php } ?>
+                            </a>
+                        <?php } ?>
+                    </div>
                 </div>
                 <div class="col-sm-8" style="overflow:scroll ">
+                    <div class="title-wrapper" style="padding:10px">
+                        <ul class="breadcrumb">
+                            <li>Noticias!</li>
+                        </ul>
+                    </div>
                     <?php for ($i = 0; $i < 4; $i++) { ?>
 
                         <div style="padding: 0;" class="card text-left post-body">
                             <div style="flex:5" class="card-body">
-                                <img style="margin: 10px; margin-top:0px; border-radius: 5px; width:95%; height: 100%" src="https://www.societyplus.net/upload/photos/2020/11/9FP6DaNMH4PU9Mth9pI4_06_219caf368060b509948cf08a0102afdc_avatar.jpg" alt="POST">
+                                <img style="object-fit:cover; margin: 10px; margin-top:0px; border-radius: 5px; width:95%; max-height:800px; max-width:800px" src="https://cataas.com/cat/says/Post!<?php echo rand(1, 5000)?>" alt="POST">
                             </div>
                             <div class="card-body" style="display:flex; flex-direction:row; flex:9">
-                                <img class="avatar" style="border-radius: 50px; height: 50px; width: 50px" src="https://www.societyplus.net/upload/photos/2020/11/9FP6DaNMH4PU9Mth9pI4_06_219caf368060b509948cf08a0102afdc_avatar.jpg" alt="">
+                                <img class="avatar" style="border-radius: 50px; height: 50px; width: 50px" src="https://cataas.com/cat/says/Profile!" alt="">
                                 <div style="display:flex; flex-direction:column; margin-left:10px; max-height: 40px">
                                     <div style="height: 50%;">
                                         <p class="card-text">@admin</p>
@@ -66,7 +80,7 @@ if (isset($_COOKIE['token'])) {                                     //SI EXISTE 
                 <div class="col-sm-1">
                     <div class="dynFlexHV Subs">
                         <?php for ($i = 0; $i < 4; $i++) { ?>
-                            <img class="avatar" style="margin-bottom: 10px; margin-left: 10px; border-radius: 50px; height: 50px; width: 50px" src="https://www.societyplus.net/upload/photos/2020/11/9FP6DaNMH4PU9Mth9pI4_06_219caf368060b509948cf08a0102afdc_avatar.jpg" alt="">
+                            <img class="avatar sub-item" style="margin-left: 10px; border-radius: 50px; height: 50px; width: 50px" src="https://cataas.com/cat" alt="">
                         <?php } ?>
                     </div>
                 </div>
