402b94ca68be8eab9291dd716e6ff7528ca85641
LiLPandemio <markhaxx@gmail.com>
2022-02-04 16:53:01 +0100

    Posts are now shown on user's profile

--- www/functions/contentEngine.php
+++ www/functions/contentEngine.php
@@ -20,6 +20,25 @@ function getPublicPosts($amount = 5)
 }
 
 /**
+ * Obtiene la cantidad de posts publicos de un usuario especifico que quieras
+ */
+function getUserPublicPosts($amount = 5, $username)
+{
+    //TODO: SANITIZE $AMOUNT
+    require(ROOT . "/functions/db.php");    //Se importa la base de datos
+    //Preparamos la consulta SQL con 5 por defecto
+    $stmt = $conn->prepare("SELECT `posts`.*, `usuarios`.* FROM `posts` LEFT JOIN `usuarios` ON `posts`.`user_id` = `usuarios`.`id_usuarios` WHERE `usuarios`.`username` = 'admin' ORDER BY `posts`.`post_id` DESC LIMIT 5;");
+    $stmt->execute(array($username, $amount));      //Se ejecuta la consulta
+    $result = $stmt->fetchAll();                    //Se exporta el resultado a un arreglo bidimensional
+    $drows = $stmt->rowCount();                     //Se cuenta cuantas filas hay
+    if ($drows < 1) {                               //Si hay menos de 1
+        return "NO_POSTS_FOUND";                    //Se indica que NO HAY POSTS encontrados
+    } else {                                        //En caso contrario
+        return $result;                             //Entregamos los posts
+    }
+}
+
+/**
  * Indica la cantidad de publicaciones que tiene un usuario (INT)
  */
 function howManyPostsHasUsername($username)
@@ -30,4 +49,14 @@ function howManyPostsHasUsername($username)
     $stmt->execute(array($username));           //Reemplaza de forma segura el nombre de usuario. 
     $drows = $stmt->rowCount();                 //Contamos cuantas filas han salido
     return $drows;                              //Retorna el valor
+}
+
+/**
+ * Indica la cantidad de publicaciones que tiene un usuario (INT)
+ */
+function displayPostByID($pid)
+{
+    ?>
+    <!-- SPAWN A POST -->
+    <?php
 }
--- www/themes/default/profile/index.php
+++ www/themes/default/profile/index.php
@@ -17,11 +17,11 @@ if (isset($_COOKIE['token'])) {                         //SI EXISTE LA COOKIE CO
             $isMyProfile = true;
             $username = whoami();
         }
-
+        $user = getUserData($username);
         //Mostrar la pagina
 ?>
         <!-- Aqui puedes añadir contenido exta a head -->
-
+        <link rel="stylesheet" href="<?= $config["fullsiteurl"] . "themes/" . $config["theme"] . "/home/" . "index" ?>.css">
         <!-- Hasta aqui el contenido head -->
         </head>
 
@@ -36,10 +36,10 @@ if (isset($_COOKIE['token'])) {                         //SI EXISTE LA COOKIE CO
                     <?php include(ROOT . "/themes/" . $config["theme"] . "/components/left-profile-suggestions.phtml") ?>
                 </div>
                 <div class="col-sm-6" style="padding-top: 10px">
-                    <div class="card text-left" style="padding: 10px">
-                        <img class="card-img-top" style="border-radius: 20px" src="https://www.societyplus.net/upload/photos/d-cover.jpg" alt="">
+                    <div class="card text-left" style="padding: 10px; margin:10px">
+                        <img class="card-img-top" style="border-radius: 20px" src="<?php echo $config['fullsiteurl'].$user['cover_picture_rpath']?>" alt="">
                         <div style="margin-top: -50px; height:110px; width:110px; object-fit: cover; margin-left: 50px; border-radius: 100px; background-color:white">
-                            <img src="https://forfans.societyplus.net//upload/pictures/admin_pfp.jpg" style="height:100px; margin-left: 5px; margin-top: 5px; width:100px; border-radius: 50px" alt="">
+                            <img src="<?php echo $config['fullsiteurl'].$user['profile_picture_rpath']?>" style="height:100px; margin-left: 5px; margin-top: 5px; width:100px; border-radius: 50px" alt="">
                         </div>
                         <div class="card-body">
                             <h4 class="card-title">@admin</h4>
@@ -49,6 +49,77 @@ if (isset($_COOKIE['token'])) {                         //SI EXISTE LA COOKIE CO
                             </p>
                         </div>
                     </div>
+
+                    <?php $posts = getUserPublicPosts(10, $username);?>
+
+                    <?php
+                    if ($posts == "NO_POSTS_FOUND") {
+                        echo "NO POSTS FOUND";
+                    } else {
+
+                        for ($i = 0; $i < count($posts); $i++) {
+                            $post = $posts[$i];
+                            $img_array = json_decode($post["post_img_array"]);
+                    ?>
+
+                            <div style="padding: 0; min-height: 160px; margin: 10px;" class="card text-left post-body">
+                                <?php
+                                if ($img_array != "") {
+                                ?>
+                                    <div style="flex:5" class="card-body">
+                                        <?php
+
+                                        for ($j = 0; $j < count($img_array); $j++) {
+                                        ?>
+                                            <img style="object-fit:cover; margin: 10px; margin-top:10px; border-radius: 5px; width:95%" src="<?php echo $img_array[$j] ?>" alt="POST">
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+
+                                <?php
+
+                                } else {
+
+                                ?>
+                                    <div style="flex:3" class=""></div>
+                                <?php
+
+                                }
+
+                                ?>
+                                <div class="card-body" style="display:flex; flex-direction:row; flex:6">
+                                    <img class="avatar" style="border-radius: 50px; height: 50px; width: 50px; margin-right:10px;" src="<?= $config["fullsiteurl"] . $post["profile_picture_rpath"] ?>" alt="">
+                                    <div style="flex-wrap: wrap;">
+                                        <div class="post-info-about" style="max-height: 40px">
+                                            <div style="height: 50%;">
+                                                <p class="card-text">@<?= $post["username"] ?></p>
+                                            </div>
+                                            <div style="height: 50%;">
+                                                <a class="badge badge-primary" href="#"><?php echo howManyPostsHasUsername($post['username']) ?> Posts</a>
+                                                <a class="badge badge-danger" href="#">Suscribirse</a>
+                                                <a class="badge badge-success" href="#">Donado $<?= $post["post_donations"] ?></a>
+                                                <a class="badge badge-accent" href="#">Seguir</a>
+                                            </div>
+                                        </div>
+                                        <div style="flex: 1 1 250px; margin-top:40px; margin-left:-50px">
+                                            <?= $post["post_text"] ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                                if ($img_array !== "") {
+                                ?>
+                                    <div style="flex:3" class=""></div>
+                                <?php
+                                }
+                                ?>
+                            </div>
+                    <?php }
+                    }
+                    ?>
+
+
                 </div>
                 <div class="col-sm-3">
 
